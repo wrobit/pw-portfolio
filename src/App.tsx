63985c7249f1d031981975bc815f38ac2673f86d
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2023-03-12 01:06:39 +0100

    add: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import { theme } from "@utils/theme";
-import styled, { ThemeProvider } from "styled-components";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { Home, Work, Contact, Error, Root } from "./pages";
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="work" element={<Work />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+      </Route>
+    )
+  );
+
   return (
     <ThemeProvider theme={theme}>
-      <Paragraph>Hello world</Paragraph>
+      <RouterProvider router={router}></RouterProvider>
     </ThemeProvider>
   );
 };
 
 export default App;
-
-const Paragraph = styled.p`
-  color: ${({ theme }) => theme.colors.white};
-`;
