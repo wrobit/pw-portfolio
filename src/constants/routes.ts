f7c2f84be285dbe6550d1928470c7e097d1e9888
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2023-04-03 22:30:07 +0200

    add: routes constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,15 @@
+const home = "/";
+const work = "/work";
+const about = "/about";
+const contact = "/contact";
+const project = "/project";
+const error404 = "/404";
+
+export const routes = {
+  home,
+  about,
+  work,
+  contact,
+  project,
+  error404
+};
