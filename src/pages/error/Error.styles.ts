d31e1844d11cab3663998364c2b8ed972cebdd6f
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2023-04-09 12:53:52 +0200

    add: init error page

---
+++ src/pages/error/Error.styles.ts
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+const ErrorContainer = styled.div`
+  height: calc(100vh - 88px);
+  width: 100%;
+  padding: 0 110px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    padding: 0 102px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    padding: 0 32px;
+  }
+`;
+
+const ErrorContentWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  transform: translateY(calc(-50%));
+  gap: 2rem;
+`;
+
+const ErrorTitle = styled.h1`
+  width: 80%;
+  font-size: 52px;
+  font-family: DMSansMedium;
+  text-align: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    width: 100%;
+    font-size: 36px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: 32px;
+  }
+`;
+
+const ErrorDisclaimer = styled.span`
+  color: ${({ theme }) => theme.colors.gray};
+`;
+
+export { ErrorContainer, ErrorContentWrapper, ErrorTitle, ErrorDisclaimer };
--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,3 +1,15 @@
+import { Button } from "@components/common";
+import * as Styled from "./Error.styles";
+
 export const Error = () => {
-  return <div>Error Page</div>;
+  return (
+    <Styled.ErrorContainer>
+      <Styled.ErrorContentWrapper>
+        <Styled.ErrorTitle>
+          Oops. It looks like the page you're trying to reach <Styled.ErrorDisclaimer>doesn't exist</Styled.ErrorDisclaimer> or has been moved.
+        </Styled.ErrorTitle>
+        <Button>Go to Home</Button>
+      </Styled.ErrorContentWrapper>
+    </Styled.ErrorContainer>
+  );
 };
