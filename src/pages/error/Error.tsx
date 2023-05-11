7f7e5e29e49c2aa0c75c6a8a9cf809e8ecc0b8c8
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2023-05-11 23:30:31 +0200

    add: error page home button navigation functionality

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,14 +1,22 @@
 import { Button } from "@components/common";
+import { routes } from "@utils/constants/routes.constants";
+import { useNavigate } from "react-router-dom";
 import * as Styled from "./Error.styles";
 
 export const Error = () => {
+  const navigate = useNavigate();
+
+  const handleRouteChange = () => {
+    navigate(routes.home);
+  };
+
   return (
     <Styled.ErrorContainer>
       <Styled.ErrorContentWrapper>
         <Styled.ErrorTitle>
           Oops. It looks like the page you're trying to reach <Styled.ErrorDisclaimer>doesn't exist</Styled.ErrorDisclaimer> or has been moved.
         </Styled.ErrorTitle>
-        <Button>Go to Home</Button>
+        <Button onClick={handleRouteChange}>Go to Home</Button>
       </Styled.ErrorContentWrapper>
     </Styled.ErrorContainer>
   );
