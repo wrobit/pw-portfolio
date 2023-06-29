5a08902233b025087cb23c9dad2d9fc13ea3057e
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2023-06-29 21:32:39 +0200

    ref: change default typography color

--- src/components/common/Typography/Typography.styles.ts
+++ src/components/common/Typography/Typography.styles.ts
@@ -4,7 +4,7 @@ import { TypographyProps } from "./Typography.types";
 const Typography = styled.span<Omit<TypographyProps, "children" | "element">>`
   font-family: ${({ theme, fontFamily }) => (fontFamily ? theme.font[fontFamily] : theme.font.regular)};
   font-size: ${({ fontSize }) => (fontSize ? fontSize : "inherit")};
-  color: ${({ theme, color }) => (color ? color : theme.colors.black)};
+  color: ${({ theme, color }) => (color ? color : theme.colors.white)};
 `;
 
 export { Typography };
