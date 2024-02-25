c711b3ea660deb6a29ccb61607576c3f57dc6162
ajiiz <opticpiter@gmail.com>
2024-02-25 17:55:17 +0100

    ref: transitions

--- src/components/common/Link/Link.styles.ts
+++ src/components/common/Link/Link.styles.ts
@@ -5,6 +5,7 @@ const Link = styled.a`
   color: ${({ theme }) => theme.colors.white};
   text-decoration: none;
   cursor: pointer;
+  transition: color 100ms linear;
 
   &:active {
     color: ${({ theme }) => hexToRgba(theme.colors.white, 0.6)};
