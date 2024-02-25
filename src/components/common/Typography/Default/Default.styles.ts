c9fa5da4553de0c40c7d8eccad4521e181d23859
ajiiz <opticpiter@gmail.com>
2024-02-25 17:54:04 +0100

    ref: default typography

--- src/components/common/Typography/Default/Default.styles.ts
+++ src/components/common/Typography/Default/Default.styles.ts
@@ -4,11 +4,6 @@ const Default = styled.span`
   color: ${({ theme, color }) => color || theme.colors.white};
   font-family: inherit;
   font-size: inherit;
-
-  &:hover,
-  &:active {
-    color: inherit;
-  }
 `;
 
 export { Default };
