75788cfee0afbcae0c24e141e9c0242429bac4f4
ajiiz <opticpiter@gmail.com>
2024-02-25 18:17:22 +0100

    ref: h1 fontsize

--- src/components/common/Typography/Headers/H1/H1.styles.ts
+++ src/components/common/Typography/Headers/H1/H1.styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const H1 = styled.h1`
   color: ${({ theme, color }) => color || theme.colors.white};
   font-family: ${({ theme }) => theme.font.medium};
-  font-size: 96px;
+  font-size: 84px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
     font-size: 64px;
