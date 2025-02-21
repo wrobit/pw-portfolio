9b40d63a262c129f4ba75a21616a3680152014ff
ajiiz <piotrwrobel.ajiiz@gmail.com>
2025-02-21 22:27:02 +0100

    feat: hero animations

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import ScrollDownIcon from "@assets/icons/expand-down-double.svg";
 import { Typography } from "@components/common";
 import * as Styled from "@components/home/Hero/Hero.styles";
+import { motion } from "framer-motion";
 import { useRef } from "react";
 import { useTheme } from "styled-components";
 
@@ -16,19 +17,32 @@ export const Hero = () => {
 
   return (
     <Styled.HeroSection ref={heroSectionRef}>
-      <Styled.HeroTypographyWrapper>
-        <Typography.Headers.H1>Software Engineer & UX / UI Designer</Typography.Headers.H1>
-        <Typography.Headers.H4>
-          Software developer based in Poland. Mostly interested in web development and UI/UX design. Person that always seek to create everything from
-          his own ideas. Looking for new opportunities and ideas.
-        </Typography.Headers.H4>
-      </Styled.HeroTypographyWrapper>
-      <Styled.HeroButtonWrapper>
-        <Styled.HeroButton onClick={scrollToExplore}>
-          <Typography.Buttons.Small color="inherit">scroll to explore</Typography.Buttons.Small>
-          <Styled.HeroButtonIcon src={ScrollDownIcon} />
-        </Styled.HeroButton>
-      </Styled.HeroButtonWrapper>
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.5 }}>
+        <Styled.HeroTypographyWrapper>
+          <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.6, delay: 0.2 }}>
+            <Typography.Headers.H1>Software Engineer & UX / UI Designer</Typography.Headers.H1>
+          </motion.div>
+          <motion.div initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.6, delay: 0.4 }}>
+            <Typography.Headers.H4>
+              Software developer based in Poland. Mostly interested in web development and UI/UX design. Person that always seek to create everything
+              from his own ideas. Looking for new opportunities and ideas.
+            </Typography.Headers.H4>
+          </motion.div>
+        </Styled.HeroTypographyWrapper>
+        <Styled.HeroButtonWrapper>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4, delay: 0.6 }}
+            whileHover={{ y: -5 }}
+          >
+            <Styled.HeroButton onClick={scrollToExplore}>
+              <Typography.Buttons.Small color="inherit">scroll to explore</Typography.Buttons.Small>
+              <Styled.HeroButtonIcon src={ScrollDownIcon} />
+            </Styled.HeroButton>
+          </motion.div>
+        </Styled.HeroButtonWrapper>
+      </motion.div>
     </Styled.HeroSection>
   );
 };
