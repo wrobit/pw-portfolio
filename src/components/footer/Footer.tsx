16618b1151db235be030ff3ecde57418127ed483
wrobit <piotrwrobel.ajiiz@gmail.com>
2025-04-15 22:56:26 +0200

    feat: footer links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,8 @@
 import { Typography } from "@components/common";
 import { useTheme } from "styled-components";
 import * as Styled from "./Footer.styles";
+import { Link } from "@components/common/Link/Link";
+import { data } from "./Footer.data";
 
 export const Footer = () => {
   const theme = useTheme();
@@ -9,11 +11,21 @@ export const Footer = () => {
   return (
     <Styled.Footer>
       <Styled.FooterNavigation>
-        <Styled.FooterNavigationHeader>
-          <Typography.Headers.H5>
-            From pixel-perfect designs to intuitive user experiences. Let's collaborate and create something remarkable together.
-          </Typography.Headers.H5>
-        </Styled.FooterNavigationHeader>
+        <Typography.Headers.H5>
+          From pixel-perfect designs to intuitive user experiences. Let's collaborate and create something remarkable together.
+        </Typography.Headers.H5>
+        <Styled.FooterNavigationLinksContainer>
+          {data.map(item => (
+            <Styled.FooterNavigationLinks key={item.header}>
+              <Styled.FooterNavigationHeaderText>{item.header.toUpperCase()}</Styled.FooterNavigationHeaderText>
+              {item.links.map(link => (
+                <Link key={link.label} href={link.href}>
+                  {link.label.charAt(0).toUpperCase() + link.label.slice(1)}
+                </Link>
+              ))}
+            </Styled.FooterNavigationLinks>
+          ))}
+        </Styled.FooterNavigationLinksContainer>
       </Styled.FooterNavigation>
       <Typography.Headers.H6 color={theme.colors.gray}>
         Copyright &copy; {`${dateYear}`}
