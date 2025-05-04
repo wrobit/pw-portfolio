2cac60960f256769bfa2143d0e243816234627c1
wrobit <piotrwrobel.ajiiz@gmail.com>
2025-05-04 11:33:43 +0200

    feat: work page hero

---
+++ src/components/work/Hero/Hero.styles.ts
@@ -0,0 +1,85 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+const HeroSection = styled(motion.div)`
+  width: 100%;
+  margin-top: ${({ theme }) => theme.spacing.xxl};
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.xxxl};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    width: 100%;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    gap: ${({ theme }) => theme.spacing.lg};
+    margin-top: ${({ theme }) => theme.spacing.lg};
+  }
+`;
+
+const HeroTypographyWrapper = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.spacing.xxl};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    width: 100%;
+  }
+`;
+
+const HeroButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeroButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: ${({ theme }) => theme.spacing.md} ${({ theme }) => theme.spacing.md};
+  gap: ${({ theme }) => theme.spacing.sm};
+  color: ${({ theme }) => theme.colors.gray};
+  border: none;
+  cursor: pointer;
+  transition: color 200ms linear;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.white};
+
+    & span {
+      color: ${({ theme }) => theme.colors.white};
+    }
+
+    & img {
+      animation-play-state: paused;
+    }
+  }
+`;
+
+const HeroButtonIcon = styled.img`
+  animation: bounce 500ms infinite linear;
+
+  @keyframes bounce {
+    0% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(-5px);
+    }
+    100% {
+      transform: translateY(0);
+    }
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    width: 26px;
+    height: 26px;
+  }
+`;
+
+export { HeroButton, HeroButtonIcon, HeroButtonWrapper, HeroSection, HeroTypographyWrapper };
--- src/pages/work/Work.tsx
+++ src/pages/work/Work.tsx
@@ -1,3 +1,10 @@
+import { PageTemplateWrapper } from "@pages/template/template.styles";
+import { Hero } from "../../components/work/Hero/Hero";
+
 export const Work = () => {
-  return <div>Work Page</div>;
+  return (
+    <PageTemplateWrapper>
+      <Hero />
+    </PageTemplateWrapper>
+  );
 };
