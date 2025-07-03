c97196ddc981eda5a26f1d9f75b6e2421ad25bca
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2025-07-03 11:10:07 +0200

    feat: h5 typography breakpoints adjustments

--- src/components/shared/Typography/Headers/H5/H5.styles.ts
+++ src/components/shared/Typography/Headers/H5/H5.styles.ts
@@ -4,6 +4,14 @@ const H5 = styled.h5`
   color: ${({ theme, color }) => color || theme.colors.white};
   font-family: ${({ theme }) => theme.font.regular};
   font-size: 20px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    font-size: 18px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: 16px;
+  }
 `;
 
 export { H5 };
