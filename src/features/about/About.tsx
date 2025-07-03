c049962da3359af3452b5798bf5702c98d4c2d55
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2025-07-03 10:19:04 +0200

    feat: hero image component with animations

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -1,11 +1,13 @@
 import { PageTemplateWrapper } from "@utils/template/template.styles";
 
 import { Hero } from "./components/Hero/Hero";
+import { HeroImage } from "./components/HeroImage/HeroImage";
 
 export const About = () => {
   return (
     <PageTemplateWrapper>
       <Hero />
+      <HeroImage />
     </PageTemplateWrapper>
   );
 };
