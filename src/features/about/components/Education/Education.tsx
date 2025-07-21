1a2aa27c5e9fa37b5ff8c4a56e61e47415e6484d
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2025-07-21 12:15:44 +0200

    feat: education section

---
+++ src/features/about/components/Education/Education.tsx
@@ -0,0 +1,43 @@
+import { motion } from "framer-motion";
+import { useRef } from "react";
+
+import { Typography } from "@components/shared";
+import { fadeInUp, pageTransition } from "@utils/animations/variants";
+
+import { timelineVariants, timelineItemVariants } from "./Education.animations";
+import { data } from "./Education.data";
+import * as Styled from "../Experience/Experience.styles";
+
+export const Education = () => {
+  const heroSectionRef = useRef<HTMLDivElement>(null);
+
+  return (
+    <Styled.ExperienceSection
+      ref={heroSectionRef}
+      variants={pageTransition}
+      initial="initial"
+      animate="animate"
+    >
+      <Styled.ExperienceTypographyWrapper>
+        <motion.div variants={fadeInUp} initial="hidden" animate="visible" custom={0.2}>
+          <Typography.Headers.H1>Experience</Typography.Headers.H1>
+        </motion.div>
+      </Styled.ExperienceTypographyWrapper>
+      <Styled.ExperienceTimeline variants={timelineVariants} initial="hidden" animate="visible">
+        {data.map((item, index) => (
+          <Styled.ExperienceTimelineRow
+            key={`${item.title}-${index}`}
+            variants={timelineItemVariants}
+            custom={index * 0.1}
+          >
+            <Styled.ExperienceTimelineHeaderItem>
+              {item.company}
+            </Styled.ExperienceTimelineHeaderItem>
+            <Styled.ExperienceTimelineItem>{item.title}</Styled.ExperienceTimelineItem>
+            <Styled.ExperienceTimelineItem>{item.date}</Styled.ExperienceTimelineItem>
+          </Styled.ExperienceTimelineRow>
+        ))}
+      </Styled.ExperienceTimeline>
+    </Styled.ExperienceSection>
+  );
+};
