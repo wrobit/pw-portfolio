4e6c5a43ae388e82911d71223c47685e6eddd542
Piotr Wrobel <piotrwrobel.ajiiz@gmail.com>
2026-01-22 10:35:17 +0100

    chore: fix vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,16 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: "/",
   build: {
     chunkSizeWarningLimit: 1600,
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
   },
 });
