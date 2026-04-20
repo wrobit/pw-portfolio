4aa94cddc2a7b6a0dbf1c8cb75397b79fac1cc8b
wrobit <piotrwrobel.ajiiz@gmail.com>
2026-04-20 15:52:36 +0200

    fix: footer logo

--- src/components/shared/typography/headers/logo/logo.styles.ts
+++ src/components/shared/typography/headers/logo/logo.styles.ts
@@ -5,6 +5,11 @@ const Logo = styled.h1`
   font-family: ${({ theme }) => theme.font.regular};
   font-size: ${({ theme }) => theme.fontSize.logo};
   line-height: ${({ theme }) => theme.fontSize.logo};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: 120px;
+    line-height: 120px;
+  }
 `;
 
 export { Logo };
